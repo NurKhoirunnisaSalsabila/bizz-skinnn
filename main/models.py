39f89ed898f441af37203ea459de60ccedefd0d2
NurKhoirunnisaSalsabila <158555962+NurKhoirunnisaSalsabila@users.noreply.github.com>
2024-09-25 05:23:05 +0700

    Update models.py

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.IntegerField(max_digits=10, decimal_places=2)
     description = models.TextField()
     # skin_type = models.CharField(max_length=50)
     # stock = models.IntegerField()
