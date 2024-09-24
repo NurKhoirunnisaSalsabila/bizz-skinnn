7b34a8ea8c8ecd2eda9d5b46240032e78cb736e4
NurKhoirunnisaSalsabila <nur.khoirunnisa@ui.ac.id>
2024-09-25 05:29:01 +0700

    update models

--- main/models.py
+++ main/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.IntegerField()
     description = models.TextField()
     # skin_type = models.CharField(max_length=50)
     # stock = models.IntegerField()
