b2b9ca68c905c68f9930811a137d8b3d8850435b
NurKhoirunnisaSalsabila <nur.khoirunnisa@ui.ac.id>
2024-09-24 17:52:25 +0700

    bisadong

---
+++ main/models.py
@@ -0,0 +1,13 @@
+import uuid
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    # skin_type = models.CharField(max_length=50)
+    # stock = models.IntegerField()
+    # rating = models.DecimalField(max_digits=3, decimal_places=2)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)  # Relasi ke User
